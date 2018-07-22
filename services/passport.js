6c9f951d855035ead002cd0a3056c66e9fbe9197
Himawan Tan <himawantan@Himawans-MacBook-Pro.local>
2018-07-22 07:32:55 +0700

    tweak google startegy setting

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
     }, (accessToken, refreshToken, profile, done) => {
         // Validator to block duplicated id
         User.findOne({googleId: profile.id})
