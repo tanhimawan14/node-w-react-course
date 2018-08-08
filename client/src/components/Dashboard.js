466f84b930a9784c249856c24354679ea064cf78
Himawan Tan <himawantan@Himawans-MacBook-Pro.local>
2018-08-08 21:19:06 +0700

    add survey UI

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Dashboard = () => {
     return (
@@ -6,9 +7,9 @@ const Dashboard = () => {
             Dashboard
 
             <div className="fixed-action-btn">
-                <a className="btn-floating btn-large red">
+                <Link to="/surveys/new" className="btn-floating btn-large red">
                     <i className="material-icons">add</i>
-                </a>
+                </Link>
             </div>
         </div>
     )
